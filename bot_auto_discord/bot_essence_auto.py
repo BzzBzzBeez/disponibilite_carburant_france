60b59d45a15f69c199a7062d06b3f02b8f91c1a5
BzzBzzBeez <55196216+BzzBzzBeez@users.noreply.github.com>
2022-10-19 19:42:10 +0200

    Modification fonctionnement
    Envoie de message sur Discord SEULEMENT si il y a eu un approvisionnement de nouveau carburant

--- bot_auto_discord/bot_essence_auto.py
+++ bot_auto_discord/bot_essence_auto.py
@@ -5,32 +5,29 @@
 
 from xml.dom import minidom
 from discord_webhook import DiscordWebhook
+from datetime import datetime
 import os
+import sys
 import wget
 import shutil
 
+CONST_OLD_NAME = "PrixCarbuOLD.xml"
+CONST_NEW_NAME = "PrixCarburants_instantane.xml"
 
-#Récupération url webhook discord
+#Check présence fichier old (Sinon renommer en old)
+if not os.path.exists(CONST_NEW_NAME):
+    print("Ancien fichier non présent, relance dans 10 minutes...")
+    sys.exit()
+else:
+    os.rename(CONST_NEW_NAME,CONST_OLD_NAME)
 
-urlwebhook = open("id.txt","r")
-url_api = str(urlwebhook.read().strip())
-urlwebhook.close()
-message = ""
-
-#Input
-
-f = open("info.txt", "r")
-lines = f.readlines()
-#print(lines)
-f.close()
-
-#Supprimer l'ancien fichier
+#Supprimer les anciens fichiers
 if os.path.exists("PrixCarburants_instantane.zip"):
     os.remove("PrixCarburants_instantane.zip")
 if os.path.exists("PrixCarburants_instantane.xml"):
     os.remove("PrixCarburants_instantane.xml")
 
-# #Téléchargement du nouveau fichier
+#Téléchargement du nouveau fichier
 remote_url="https://donnees.roulez-eco.fr/opendata/instantane"
 local_file="PrixCarburants_instantane.zip"
 wget.download(remote_url,local_file)
@@ -39,22 +36,68 @@ print("\n")
 #Dezipage fichier
 shutil.unpack_archive("PrixCarburants_instantane.zip")
 
-#Récupération Prix Carburants Bois d'Arcy
-doc = minidom.parse("PrixCarburants_instantane.xml")
+#Récupération du Code Postal
+if os.path.exists("info.txt"):
+    f = open("info.txt", "r")
+    lines = f.readlines()
+    f.close()
+else:
+    print("Fichier 'info.txt' n'éxiste pas...")
+    sys.exit()
+
+#Récupération URL webhook discord
+if os.path.exists("id.txt"):
+    urlwebhook = open("id.txt","r")
+    url_api = str(urlwebhook.read().strip())
+    urlwebhook.close()
+else:
+    print("Fichier 'id.txt' n'éxiste pas...")
+
+#Récupération Prix Carburants OLD
+doc = minidom.parse(CONST_OLD_NAME)
 pdvs = doc.getElementsByTagName("pdv")
 
-message = "```"
+OLD_tab=[]
+
 for line in lines:
     for pdv in pdvs:
         if pdv.getAttribute("cp") == line.strip():
             for i in pdv.getElementsByTagName("prix"):
-                adresse = pdv.getElementsByTagName("adresse")[0].childNodes[0].data
                 ville = pdv.getElementsByTagName("ville")[0].childNodes[0].data
-                nom = str(i.attributes["nom"].value)
-                prix = str(i.attributes["valeur"].value)
-                message += (ville + " : " + adresse + " : " + nom + " : " + prix + "\n")
-                print(ville + " : " + adresse + " : " + nom + " : " + prix)
-    print("\t")
-message += '```\n <@PUT_DISC_USER_ID>' #To be tagged
-webhook = DiscordWebhook(url=url_api, content=message)
-response = webhook.execute()
+                OLD_tab.append(pdv.getElementsByTagName("ville")[0].childNodes[0].data + " : " + pdv.getElementsByTagName("adresse")[0].childNodes[0].data  + " : " + str(i.attributes["nom"].value))
+
+#Récupération Prix Carburants NEW
+doc = minidom.parse(CONST_NEW_NAME)
+pdvs = doc.getElementsByTagName("pdv")
+
+NEW_tab=[]
+
+for line in lines:
+    for pdv in pdvs:
+        if pdv.getAttribute("cp") == line.strip():
+            for i in pdv.getElementsByTagName("prix"):
+                ville = pdv.getElementsByTagName("ville")[0].childNodes[0].data
+                NEW_tab.append(pdv.getElementsByTagName("ville")[0].childNodes[0].data + " : " + pdv.getElementsByTagName("adresse")[0].childNodes[0].data  + " : " + str(i.attributes["nom"].value))
+
+#Comparaison Prix Carburant OLD et NEW
+liste_carb = ""
+diff = set(NEW_tab).difference(OLD_tab)
+for i in diff:
+    liste_carb += (str(i) + "\n")
+
+#Envoie sur le channel Discord si ajout d'un carburant
+if not liste_carb == "":
+    print(liste_carb)
+    webhook = DiscordWebhook(url=url_api, content=liste_carb)
+    response = webhook.execute()
+else:
+    print("Il n'y a pas eu d'ajout de carburant :'(")
+
+#Affichage de la date dans le cas de log cronjob
+now = datetime.now()
+date_time = now.strftime("%H:%M:%S -- %m/%d/%Y")
+
+print(date_time + "\n")
+
+liste_carb = "```" + liste_carb #Mise en forme discord
+liste_carb += "```<@PUT_DISC_USER_ID>" #Put User ID to be tag
